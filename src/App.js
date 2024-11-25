0a6a70983e421d2290239a2d5bc5a0119a65cf0e
annalandi1 <landianna.1106@gmail.com>
2024-11-25 11:56:13 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+// sembra HTML , ms in realtá é una sintassi chiamata JSX
 
+// differenze principali:
+// -le classi CSS si applicamo con la parola "className" e non "class"
+// -si posssono interpolare le variabili semplicemente con {}
 function App() {
   return (
     <div className="App">
